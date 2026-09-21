50c4e491d6d10f7a15790f8b58279e4f5c3a8d31
Devfection <8330@cygnific.com>
2026-09-21 12:18:21 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import "bootstrap/dist/css/bootstrap.css"
+import './App.css'
+
+export default function App() {
+
+  return (
+    < div className="App">
+      <div className="container">
+      <h1>Weather App React</h1>
+    <footer>
+      This project was built by Arletta Niedzwiedz and is open-sourced {" "}
+      <a href="https://github.com/devfection" target="_blank"> on GitHub</a>
+</footer>
+</div>
+    </div>
+  )
+}
